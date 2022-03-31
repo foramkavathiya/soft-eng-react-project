0c150e57d608458b4d2266e6160cd4df91f75220
foramkavathiya <foramkavathiya@gmail.com>
2022-03-31 12:13:32 -0400

    Adding dislike screen

---
+++ src/components/profile/my-dislikes.js
@@ -0,0 +1,18 @@
+import Tuits from "../tuits";
+import * as service from "../../services/likes-service";
+import {useEffect, useState} from "react";
+
+const MyDislikes = () => {
+    const [dislikedTuits, setDislikedTuits] = useState([]);
+    const findTuitsDislike = () =>
+        service.findAllTuitsDislikedByUser('me')
+            .then((tuits) => setDislikedTuits(tuits));
+    useEffect(findTuitsDislike, []);
+
+    return (
+        <div>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsDislike}/>
+        </div>
+    );
+};
+export default MyDislikes;
